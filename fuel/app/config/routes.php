6c0e49d9a59457d0ed7e2adf15a21acc3f3a4ace
Naoto Mawatari <github.mawatari@gmail.com>
2012-09-07 15:48:50 +0900

    404関連のファイル修正

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	'_root_'  => 'form/index',  // The default route
-	'_404_'   => 'form/404',    // The main 404 route
+	'_404_'   => 'error/404',    // The main 404 route
 
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
