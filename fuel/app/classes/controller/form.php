d65a0dd986eda50a8071ee4187f46f9533643c68
Naoto Mawatari <github.mawatari@gmail.com>
2012-09-07 15:02:17 +0900

    完了ページのアクションメソッドを作成

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -49,4 +49,53 @@ class Controller_form extends Controller_Template
 		}
 	}
 
+	public function action_send()
+	{
+		// CSRF対策
+		if ( ! Security::check_token())
+		{
+			return 'ページ遷移が正しくありません。';
+		}
+
+		$val = $this->get_validation();
+
+		if ( ! $val->run())
+		{
+			$this->template->title = 'コンタクトフォーム：エラー';
+			$this->template->content = View::forge('form/index');
+			$this->template->content->set_safe('html_error', $val->show_errors());
+			return;
+		}
+
+		$post = $val->validated();
+		$data = $this->build_mail($post);
+
+		// メールの送信
+		try
+		{
+			$this->sendmail($data);
+			$this->template->title = 'コンタクトフォーム：送信完了';
+			$this->template->content = View::forge('form/send');
+			return;
+		}
+		catch (EmailValidationFailedException $e)
+		{
+			Log::error(
+				'メール検証エラー：' . $e->getMessage(), __METHOD__
+			);
+			$html_error = '<p>メールアドレスに誤りがあります。</p>';
+		}
+		catch (EmailSendingFailedException $e)
+		{
+			Log::error(
+				'メール送信エラー：' . $e->getMessage(), __METHOD__
+			);
+			$html_error = '<p>メールを送信できませんでした。</p>';
+		}
+
+		$this->template->title = 'コンタクトフォーム：送信エラー';
+		$this->template->content = View::forge('form/index');
+		$this->template->content->set_safe('html_error', $html_error);
+	}
+
 }
