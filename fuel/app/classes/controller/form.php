d7c6d6322d9f35a587b4367766cfbf1a05e17619
Naoto Mawatari <github.mawatari@gmail.com>
2012-09-07 14:37:43 +0900

    問い合わせフォームのコントローラを作成 インデックスのアクションメソッドを作成

---
+++ fuel/app/classes/controller/form.php
@@ -0,0 +1,11 @@
+<?php
+
+class Controller_form extends Controller_Template
+{
+	public function action_index()
+	{
+		$this->template->title = 'コンタクトフォーム';
+		$this->template->content = View::forge('form/index');
+	}
+
+}
