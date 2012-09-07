143e4aa006c6ba70c56c535484919090e1aeaf53
Naoto Mawatari <github.mawatari@gmail.com>
2012-09-07 14:41:26 +0900

    問い合わせフォームのvalidationメソッドを作成

--- fuel/app/classes/controller/form.php
+++ fuel/app/classes/controller/form.php
@@ -8,4 +8,27 @@ class Controller_form extends Controller_Template
 		$this->template->content = View::forge('form/index');
 	}
 
+	// 検証ルールの定義
+	public function get_validation()
+	{
+		$val = Validation::forge();
+
+		$val->add('name', '名前')
+			->add_rule('trim')
+			->add_rule('required')
+			->add_rule('max_length', 50);
+
+		$val->add('email', 'メールアドレス')
+			->add_rule('trim')
+			->add_rule('required')
+			->add_rule('max_length', 100)
+			->add_rule('valid_email');
+
+		$val->add('comment', 'コメント')
+			->add_rule('required')
+			->add_rule('max_length', 400);
+
+		return $val;
+	}
+
 }
