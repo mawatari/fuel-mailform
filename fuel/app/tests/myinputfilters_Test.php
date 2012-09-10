420cfed4382ced7572a384ccaa26f38ce4c0bd92
Naoto Mawatari <github.mawatari@gmail.com>
2012-09-10 16:07:29 +0900

    文字エンコーディング検証のテストを作成

---
+++ fuel/app/tests/myinputfilters_Test.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+* MyInputFilters class Tests
+*
+* @group App
+*/
+class Test_MyInputFilters extends TestCase
+{
+	public function test_check_encoding_invalid_sjis()
+	{
+		$this->setExpectedException(
+			'HttpInvalidInputException', 'Invalid input data'
+		);
+
+		$input = mb_convert_encoding('SJISの文字列です。', 'SJIS');
+		$test = MyInputFilters::check_encoding($input);
+	}
+
+	public function test_check_encoding_valid()
+	{
+		$input = '正常なUTF-8の文字列です。';
+		$test = MyInputFilters::check_encoding($input);
+		$expected = $input;
+
+		$this->assertEquals($expected, $test);
+	}
+}
