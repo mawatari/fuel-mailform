b084e44fb72e9b3ba18a1bf94cd5545f35cdd526
Naoto Mawatari <github.mawatari@gmail.com>
2012-09-12 13:12:12 +0900

    Mode_Formクラスのテストを追加 データ保存のテスト

--- fuel/app/tests/model/form_Test.php
+++ fuel/app/tests/model/form_Test.php
@@ -8,6 +8,13 @@
 
 class Test_Model_Form extends DbTestCase
 {
+	public function test_テスト環境であるか()
+	{
+		$test = Fuel::$env;
+		$expected = 'test';
+		$this->assertEquals($expected, $test);
+	}
+
 	protected $tables = array(
 		// テーブル名 => YAMLファイル名
 		'form' => 'form',
@@ -24,6 +31,30 @@ class Test_Model_Form extends DbTestCase
 				$expected = $row[$field];
 				$this->assertEquals($expected, $test);
 			}
+		}
+	}
+
+	public function test_save_insert()
+	{
+		$data = array(
+			'name'       => '藤原義孝',
+			'email'      => 'yoshitaka@example.jp',
+			'comment'    => '君がため 惜しからざりし 命さえ 長くもがなと 思ひけるかな',
+			'ip_address' => '10.11.12.13',
+			'user_agent' => 'Mozilla/2.02 (Macintosh; I; PPC)',
+		);
+
+		$form = Model_Form::forge()->set($data);
+
+		// 新規データをDBに保存
+		list($id, $rows) = $form->save();
+
+		// 保存されたデータをDBから検索
+		$form = Model_Form::find_by_pk($id);
+
+		foreach ($data as $field => $value)
+		{
+			$this->assertEquals($value, $form[$field]);
 		}
 	}
 }
